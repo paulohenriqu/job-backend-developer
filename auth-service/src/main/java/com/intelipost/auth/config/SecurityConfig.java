b279ba26f45b8ef799f919bb095fc0a609131593
Paulo Henrique de Siqueira <paulohenriqu@hotmail.com>
2019-03-24 20:11:37 -0300

    [PHS] - configuração global de segurança da aplicação

---
+++ auth-service/src/main/java/com/intelipost/auth/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.intelipost.auth.config;
+
+import com.intelipost.auth.security.TokenConfig;
+import com.intelipost.auth.security.TokenProvider;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    TokenProvider tokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {       
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .authorizeRequests()
+                .antMatchers("/auth/login").permitAll()               
+                .anyRequest().authenticated()
+            .and()
+            .apply(new TokenConfig(tokenProvider));      
+    }
+    
+}
