df962cbd6ec9e9e60c4e6388be1290256857ff44
Paulo Henrique de Siqueira <paulohenriqu@hotmail.com>
2019-03-25 14:20:07 -0300

    [PHS] - Configurações para o micro-serviço profile ser registrado no gateway

--- auth-service/src/main/java/com/intelipost/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/intelipost/auth/config/SecurityConfig.java
@@ -6,12 +6,17 @@ import com.intelipost.auth.security.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -29,10 +34,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().disable().csrf().disable().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
                 .antMatchers("/auth/login").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().authenticated()                
                 .and()
                 .apply(new TokenConfig(tokenProvider));
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {       
+        web.ignoring().antMatchers("/*/")
+                .antMatchers("/eureka/**")
+                .antMatchers(HttpMethod.OPTIONS, "/**"); 
+    }
+
+    //configuração para aceitar Cors
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addExposedHeader("X-Total-Count");
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
 
     @Bean
     PasswordEncoder getEncoder() {
