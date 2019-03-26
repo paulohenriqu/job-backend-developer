94317500809c16ab2afb10e9c62dcc6704868f9b
Paulo Henrique de Siqueira <paulohenriqu@hotmail.com>
2019-03-25 23:07:31 -0300

    [PHS] - verificação se a base de dados já foi inicializada anteriormente

--- auth-service/src/main/java/com/intelipost/auth/DataBaseInitializer.java
+++ auth-service/src/main/java/com/intelipost/auth/DataBaseInitializer.java
@@ -1,6 +1,7 @@
 package com.intelipost.auth;
 
 import java.util.Arrays;
+import java.util.List;
 
 import com.intelipost.auth.domain.User;
 import com.intelipost.auth.repository.UserRepository;
@@ -23,15 +24,19 @@ public class DataBaseInitializer implements CommandLineRunner {
 
     Logger logger = LoggerFactory.getLogger(DataBaseInitializer.class);
 
-
     @Override
     public void run(String... args) throws Exception {
 
-        
-        logger.info("Initializing sample user");
-        User user = new User("admin", this.passwordEncoder.encode("password"), Arrays.asList("ROLE_USER", "ROLE_ADMIN"));
-        this.userRepository.save(user);
-        
+        //em um caso de uso mais complexo o correto seria utilizar uma ferramenta como o Liquibase
+        List<User> check = userRepository.findAll();
+        if (check.size() > 0) {
+            logger.info("Database already initialized");
+        } else {
+            logger.info("Initializing sample user");
+            User user = new User("admin", this.passwordEncoder.encode("password"),
+                    Arrays.asList("ROLE_USER", "ROLE_ADMIN"));
+            this.userRepository.save(user);
+        }
 
     }
 }
--- profile-service/src/main/java/com/intelipost/profile/DataBaseInitializer.java
+++ profile-service/src/main/java/com/intelipost/profile/DataBaseInitializer.java
@@ -2,6 +2,7 @@ package com.intelipost.profile;
 
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
 
 import com.intelipost.profile.domain.Profile;
 import com.intelipost.profile.repository.ProfileRepository;
@@ -17,25 +18,31 @@ public class DataBaseInitializer implements CommandLineRunner {
 
     @Autowired
     ProfileRepository profileRepository;
-    
 
     Logger logger = LoggerFactory.getLogger(DataBaseInitializer.class);
-
 
     @Override
     public void run(String... args) throws Exception {
 
-        
-        logger.info("Initializing sample profiles");
-       
-        LocalDate birthday = LocalDate.of(1992, 9, 19);
+        // em um caso de uso mais complexo o correto seria utilizar uma ferramenta como
+        // o Liquibase
+        List<Profile> check = profileRepository.findAll();
+        if (check.size() > 0) {
+            logger.info("Database already initialized");
+        } else {
 
-        Profile profile=new Profile(1l, "paulohenriqu@hotmail.com", "Paulo", "de Siqueira","http://phsiqueira.com/wp-content/uploads/2018/08/profile-wp.png", birthday);
-        this.profileRepository.save(profile);
+            logger.info("Initializing sample profiles");
 
-        Profile profile2=new Profile(2l, "siqueira.pauloh@gmail.com", "Henrique", "de Siqueira","http://phsiqueira.com/wp-content/uploads/2018/08/profile-wp.png", birthday);
-        this.profileRepository.save(profile);
-        
+            LocalDate birthday = LocalDate.of(1992, 9, 19);
+
+            Profile profile = new Profile(1l, "paulohenriqu@hotmail.com", "Paulo", "de Siqueira",
+                    "http://phsiqueira.com/wp-content/uploads/2018/08/profile-wp.png", birthday);
+            this.profileRepository.save(profile);
+
+            Profile profile2 = new Profile(2l, "siqueira.pauloh@gmail.com", "Henrique", "de Siqueira",
+                    "http://phsiqueira.com/wp-content/uploads/2018/08/profile-wp.png", birthday);
+            this.profileRepository.save(profile);
+        }
 
     }
 }
